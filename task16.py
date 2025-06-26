b543c110f9e692cd62cebc49ee85f1872eeefacd
settarovartur <settarovartur2@gamil.com>
2025-06-26 18:40:21 +0500

    bajarildi

--- task16.py
+++ task16.py
@@ -1,3 +1,5 @@
 data = [123, "apple", "banana", "cat", 456, "mango", "elephant"]
-uzunligi = sorted(data, key=lambda x : len(str(x)))
+faqat_string = list(filter(lambda x: isinstance(x, str), data))
+
+uzunligi = sorted(faqat_string, key=lambda x: len(x))
 print(uzunligi)
